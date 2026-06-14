71d45c63eb77e685ef9e816bfe07a958edee7872
vinsa61 <vinsarioshen610@gmail.com>
2026-06-14 21:42:31 +0700

    feat: add getCartBadgeCount and getCartItemName methods to CartPage

---
+++ src/test/java/com/periplus/pages/CartPage.java
@@ -0,0 +1,25 @@
+package com.periplus.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class CartPage {
+    private final WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public Integer getCartBadgeCount() {
+        WebElement count = driver.findElement(By.id("cart_total"));
+        return Integer.parseInt(count.getText());
+    }
+
+    public String getCartItemName() {
+        List<WebElement> names = driver.findElements(By.className("product-name"));
+        return names.getFirst().getText();
+    }
+}
