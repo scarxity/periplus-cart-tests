9223711bbe6a129e1b989130242e6e5ce65ca3d2
vinsa61 <vinsarioshen610@gmail.com>
2026-06-14 19:43:32 +0700

    feat: initial commit

---
+++ src/main/java/com/periplus/App.java
@@ -0,0 +1,13 @@
+package com.periplus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
